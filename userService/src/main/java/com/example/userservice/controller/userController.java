440e3315fa2137d78bf4814572d1abcadb827546
육영현 <yyh6066@tsway.co.kr>
2024-09-12 20:47:44 +0900

    유저서비스1 | 구글 캘린더 일정생성 예시 추가

--- userService/src/main/java/com/example/userservice/controller/userController.java
+++ userService/src/main/java/com/example/userservice/controller/userController.java
@@ -3,13 +3,13 @@ package com.example.userservice.controller;
 import com.example.userservice.model.userVO;
 import com.example.userservice.service.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/users")
 public class userController {
@@ -17,6 +17,11 @@ public class userController {
     @Autowired
     private userRepository userService;
 
+    @PostMapping("/createEvent")
+    public void createEvent() throws GeneralSecurityException, IOException {
+        userService.createEvent();
+        System.out.println();
+    }
     @GetMapping("/{id}")
     public userVO getUser(@PathVariable String id) {
         return userService.getUserById(id);
