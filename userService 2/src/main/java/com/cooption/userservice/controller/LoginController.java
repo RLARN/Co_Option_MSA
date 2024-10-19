f58b953d5b8c58891c58029a8e99764b873a9720
최현종 <tasd7070@naver.com>
2024-10-19 18:26:07 +0900

    로그인 확인 로직 수정

--- userService 2/src/main/java/com/cooption/userservice/controller/LoginController.java
+++ userService 2/src/main/java/com/cooption/userservice/controller/LoginController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/coOption")
@@ -31,24 +32,21 @@ public class LoginController {
     private UserService userService;
     
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody String loginRequest, HttpServletRequest request) throws GeneralSecurityException, IOException{
+    public ResponseEntity<Map<String, String>> login(@RequestBody String loginRequest, HttpServletRequest request) throws GeneralSecurityException, IOException{
         
+    	Map<String, String> responseBody = new HashMap<>();
     	ObjectMapper mapper = new ObjectMapper();
     	UserVO parsinguserVO = mapper.readValue(loginRequest, UserVO.class);
     	UserVO userVO = userService.isValidUser(parsinguserVO);
     	// 로그인 처리 로직
         if (userVO == null) {  // 유효한 사용자 확인
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("아이디 또는 비밀번호가 잘못되었습니다.");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
         }else {
-        	
-        	// 로그인 성공 처리
-	        HttpSession session = request.getSession();
-	        session.setAttribute("userId", userVO.getUserId());
-	        session.setAttribute("userName", userVO.getUserName());
-	
-	        return ResponseEntity.ok("로그인 성공");
-        	
+
+            responseBody.put("userId", userVO.getUserId());
+            responseBody.put("userName", userVO.getUserName());
+
+            return ResponseEntity.ok(responseBody);
         }
 
         
